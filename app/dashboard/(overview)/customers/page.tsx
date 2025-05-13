7226096dc87b335c56fa23d6211aeb3b7dd06142
Kritsakorn <Kungruckker@gmail.com>
2025-05-13 11:15:24 +0700

    feat(loading): add loading components and delay to customers page
    Introduce loading components for the main app and invoices section to improve user experience during data fetching. Add a 1-second delay to the customers page to simulate loading behavior and demonstrate the loading state

--- app/dashboard/(overview)/customers/page.tsx
+++ app/dashboard/(overview)/customers/page.tsx
@@ -1,8 +1,14 @@
-import { Metadata } from 'next';
-export const metadata: Metadata = {
-    title: 'Customers',
-  };
+async function delay(ms: number) {
+  await new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-export default function Customers() {
-    return <p>Customers Page</p>;
-  }
+export default async function Customers() {
+  await delay(1000); // 1 second delay
+  
+  return (
+    <div className="w-full">
+      <h1 className="text-2xl">Customers</h1>
+      <p>This page took 1 second to load.</p>
+    </div>
+  );
+}
---
+++ app/dashboard/(overview)/invoices/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold">Loading...</h1>
+    </div>
+  );
+  }
